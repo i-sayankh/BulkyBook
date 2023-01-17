6844bdff489d36fd77bf63941b62b09e94ef3ded
Sayan Khutia <sayank.khutia123@gmail.com>
2023-01-17 17:28:43 +0530

    TempData and Partial Views

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -39,6 +39,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
 
@@ -77,6 +78,7 @@ namespace BulkyBookWeb.Controllers
             {
                 _db.categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
 
@@ -114,6 +116,7 @@ namespace BulkyBookWeb.Controllers
             
             _db.categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
