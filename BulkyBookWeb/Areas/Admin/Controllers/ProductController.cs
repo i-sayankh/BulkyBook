541583d54c82ab5d8107921d07187762ecc397be
Sayan Khutia <sayank.khutia123@gmail.com>
2023-02-01 16:50:19 +0530

    ViewData in .NET Core

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -43,6 +43,7 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             {
                 //create product
                 ViewBag.CategoryList = CategoryList;
+                ViewData["CoverTypeList"] = CoverTypeList;
                 return View(product);
             }
             else
