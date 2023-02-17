cef242db774436fe026477f20171e99853a52dcf
Sayan Khutia <sayank.khutia123@gmail.com>
2023-02-17 13:21:08 +0530

    Scaffold Identity

--- BulkyBook.DataAccess/Data/ApplicationDBContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDBContext.cs
@@ -1,9 +1,10 @@
 ﻿using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBook.DataAccess
 {
-    public class ApplicationDBContext: DbContext
+    public class ApplicationDBContext: IdentityDbContext
     {
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
 
