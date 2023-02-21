0f1b05b157c48a177f28e97eaffd02c0b390f876
Sayan Khutia <sayank.khutia123@gmail.com>
2023-02-21 20:05:40 +0530

    Add ShoppingCart and ApplicationUser Repository

---
+++ BulkyBook.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDBContext _db;
+
+        public ShoppingCartRepository(ApplicationDBContext db) : base(db) 
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }        
+    }
+}
