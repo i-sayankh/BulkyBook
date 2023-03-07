b1f12335b41d7920a9924f5001b068da4700f674
Sayan Khutia <sayank.khutia123@gmail.com>
2023-03-07 13:09:53 +0530

    Call Stripe from a Summary Page

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using BulkyBook.Models.ViewModels;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
@@ -109,9 +110,44 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
                 _unitOfWork.OrderDetail.Add(orderDetail);
                 _unitOfWork.Save();
 			}
-            _unitOfWork.ShoppingCart.RemoveRange(ShoppingCartVM.ListCart);
-            _unitOfWork.Save();
-			return RedirectToAction("Index", "Home");
+
+            //stripesettings
+            var domain = "https://localhost:44323/";
+			var options = new SessionCreateOptions
+			{
+                PaymentMethodTypes = new List<string>
+                {
+                    "card",
+                },
+				LineItems = new List<SessionLineItemOptions>(),
+				Mode = "payment",
+				SuccessUrl = domain+$"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+				CancelUrl = domain+$"customer/cart/index",
+			};
+
+            foreach(var item in ShoppingCartVM.ListCart)
+            {
+                var sessionLineItem = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (int)(item.Price * 100),
+                        Currency = "INR",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Title,
+                        },
+                    },
+                    Quantity = item.Count,
+                };
+                options.LineItems.Add(sessionLineItem);
+            }
+
+			var service = new SessionService();
+			Session session = service.Create(options);
+
+			Response.Headers.Add("Location", session.Url);
+			return new StatusCodeResult(303);			
 		}
 
 		public IActionResult Plus(int cartId)
