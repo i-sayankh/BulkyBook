9ae337e15c72964e58f75d02fa165e8ebcadf527
Sayan Khutia <sayank.khutia123@gmail.com>
2023-03-17 14:07:07 +0530

    DbInitializer Interface

---
+++ BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public void Initialize()
+        {
+            //migrations if they are not applied
+
+            //create roles if they are not craeted
+
+            //if roles are not created, then we will create admin user as well
+        }
+    }
+}
