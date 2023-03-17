33ba3d97a4d406e420383e68b80795a9c28677a4
Sayan Khutia <sayank.khutia123@gmail.com>
2023-03-17 15:45:29 +0530

    Implement DbInitializer

--- BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
+++ BulkyBook.DataAccess/DbInitializer/DbInitializer.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,14 +13,57 @@ using System.Threading.Tasks;
 namespace BulkyBook.DataAccess.DbInitializer
 {
     public class DbInitializer : IDbInitializer
-    {
+    {        
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDBContext _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,            
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDBContext db)
+        {
+            _userManager = userManager;            
+            _roleManager = roleManager;
+            _db = db;
+        }
         public void Initialize()
         {
             //migrations if they are not applied
+            try
+            {
+                if(_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception ex) { }
 
             //create roles if they are not craeted
+            if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Indi)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_User_Comp)).GetAwaiter().GetResult();
 
-            //if roles are not created, then we will create admin user as well
+                //if roles are not created, then we will create admin user as well
+                _userManager.CreateAsync(new ApplicationUser
+                {
+                    UserName = "admin@gmailcom",
+                    Email = "admin@gmail.com",
+                    Name = "Sayan Khutia",
+                    PhoneNumber = "8373854164",
+                    StreetAddress = "Station Road",
+                    State = "West Bengal",
+                    PostalCode = "713364",
+                    City = "Rupnarayanpur"
+                },  "Test@123").GetAwaiter().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@gmail.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+            return;
         }
     }
 }
--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using BulkyBook.Utility;
 using Stripe;
+using BulkyBook.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,7 @@ builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Str
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
     .AddEntityFrameworkStores<ApplicationDBContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddAuthentication().AddFacebook(options =>{
@@ -53,7 +55,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
-app.UseAuthentication();;
+SeedDatabase();
+app.UseAuthentication();
 
 app.UseAuthorization();
 app.UseSession();
@@ -63,3 +66,11 @@ app.MapControllerRoute(
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+void SeedDatabase()
+{
+    using(var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<DbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
